5cecc840ac68dfc954e26c04b39891c9a3bfaeeb
Susana deOliveira <33622401+colorscodex@users.noreply.github.com>
2018-04-09 15:06:28 +0000

    Create design.js

---
+++ design.js
@@ -0,0 +1,24 @@
+/* test area: myCard.
+===================== */
+
+const myCard = document.getElementById("card-back");
+
+function myFunctionTest () {
+
+  myCard.style.transform = "rotateY(180deg)";
+  myCard.style.WebkitTransform = "rotateY(180deg)";
+}
+
+myCard.addEventListener("click", myFunctionTest);
+
+
+const myButton = document.getElementById('turn-back');
+
+function myTurnBack(){
+  
+  myCard.style.transform = "rotateY(360deg)";
+  myCard.style.WebkitTransform = "rotateY(360deg)";
+}
+
+myButton.addEventListener("click", myTurnBack);
+  
